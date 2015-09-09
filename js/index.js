2cb9350c20f14243af6f7cd02c16dd670521d954
clarer26 <clare@nativetechnolgy.com.au>
2015-09-09 18:06:53 +1000

    finished read more / read less

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-$('.readmore').click(function() {
-    $.show('.readless hide');
-}
+$('.readmore a').click(function(e) {
+    e.preventDefault();
+    console.log('readmore');
+    $('.hide').show('.readless hide');
+    $('.readmore a').hide('.readmore a');
+});
 
+$('.readless a').click(function(e) {
+    e.preventDefault();
+    console.log('readless');
+    $('.hide').hide('.readless hide');
+    $('.readmore a').show('.readmore a');
+});
+
+$('.learnmore a').click(function(e) {
+    e.preventDefault();
+    console.log('learnmore');
+    //$('.hide').show('.readless hide');
+    //$('.readmore a').hide('.readmore a');
+});
 
 
 /*
