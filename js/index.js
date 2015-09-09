23fbcd2abc3c2f5299bd05e1b05f38908cc2c0e7
clarer26 <clare@nativetechnolgy.com.au>
2015-09-09 19:31:30 +1000

    changed console log notes

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
+
 $('.readmore a').click(function(e) {
     e.preventDefault();
-    console.log('readmore');
+    console.log('I\'m reading more!');
     $('.hide').show('.readless hide');
+    $('.show-this-on-click').slideDown('p');
     $('.readmore a').hide('.readmore a');
 });
 
 $('.readless a').click(function(e) {
     e.preventDefault();
-    console.log('readless');
+    console.log('I\'m reading less!');
     $('.hide').hide('.readless hide');
+    $('.show-this-on-click').slideUp('p');
     $('.readmore a').show('.readmore a');
 });
 
 $('.learnmore a').click(function(e) {
     e.preventDefault();
-    console.log('learnmore');
+    console.log('I\'m learning more!');
     //$('.hide').show('.readless hide');
     //$('.readmore a').hide('.readmore a');
 });
