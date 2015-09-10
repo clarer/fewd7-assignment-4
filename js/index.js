ffdb990a5776f75cf1aa9d5de308c108131cc8fe
clarer26 <clare@nativetechnolgy.com.au>
2015-09-10 21:40:57 +1000

    final

--- js/index.js
+++ js/index.js
@@ -2,24 +2,26 @@ $(document).ready(function() {
 
 
     $('.readmore a').click(function(e) {
-    e.preventDefault();
-    console.log('I\'m reading more!');
-    $('.readmore').hide();
-    $('#show-this-on-click').slideDown('slow');
-    $('.hide').show('.readless hide');
+        e.preventDefault();
+        console.log('I\'m reading more!');
+        $('.readmore').hide();
+        $('#show-this-on-click').slideDown('slow');
+        $('.hide').show('.readless hide');
     });
 
     $('.readless a').click(function(e) {
-    e.preventDefault();
-    console.log('I\'m reading less!');
-    $('.readmore').show();
-    $('#show-this-on-click').slideUp('slow');
-    $('.hide').hide('.readless hide');
+        e.preventDefault();
+        console.log('I\'m reading less!');
+        $('.readmore').show();
+        $('#show-this-on-click').slideUp('slow');
+        $('.hide').hide('.readless hide');
     });
 
     $('a.learnmore').click(function(e) {
-    e.preventDefault();
-    console.log('I\'m learning more!');
+        e.preventDefault();
+        console.log('I\'m learning more!');
+        $('span.hide').slideDown();
+        $('.learnmore').hide();
     });
 
 });
